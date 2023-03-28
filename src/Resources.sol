3879b42e4c3789ea460982207890f81a9ec22cfb
Alex Metelli <alex-metelli@gmx.com>
2023-03-28 13:57:17 +0100

    added storage

---
+++ src/Resources.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Resources {
+
+    address private noGameAddr;
+
+    uint256 private timeLock;
+
+    uint256 private resourceQued;
+
+    
+
+}
