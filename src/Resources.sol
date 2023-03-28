5e590f241e35f94a34f6690913b09c150a42c32e
Alex Metelli <alex-metelli@gmx.com>
2023-03-28 14:00:11 +0100

    added constants

--- src/Resources.sol
+++ src/Resources.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.13;
 
 contract Resources {
 
+    uint256 private constant METAL_MINE = 41;
+    uint256 private constant CRYSTAL_MINE = 42;
+    uint256 private constant DEUTERIUM_MINE = 43;
+    uint256 private constant SOLAR_PLANT = 44;
+    
+
     address private noGameAddr;
 
     uint256 private timeLock;
