e770a1782072cb2b4cdee0133c969b057df265b3
Alex Metelli <alex-metelli@gmx.com>
2023-03-29 12:45:53 +0100

    chore run formatter

--- src/Resources.sol
+++ src/Resources.sol
@@ -4,54 +4,62 @@ pragma solidity ^0.8.13;
 import {Utils} from "./libs/Libraries.sol";
 
 contract Resources {
-
     address private noGameAddr;
 
     constructor(address _noGameAddr) {
         noGameAddr = _noGameAddr;
     }
 
-    function upgradesCost(address caller) public view returns(uint metalMine, uint crystalMine, uint deuteriumMine, uint SolarPlant) {
-        
-    }
+    function upgradesCost(
+        address caller
+    )
+        public
+        view
+        returns (
+            uint metalMine,
+            uint crystalMine,
+            uint deuteriumMine,
+            uint SolarPlant
+        )
+    {}
 
     //#############################################################################################
     //                                   EXTERNAL FUNCTIONS                                        #
     // ############################################################################################
 
-    function steelMineUpgrade(address caller) external returns (uint metalCost, uint crystalCost, uint timeUnlocked) {
+    function steelMineUpgrade(
+        address caller
+    ) external returns (uint metalCost, uint crystalCost, uint timeUnlocked) {}
 
-    }
+    function quarzMineUpgrade(
+        address caller
+    ) external returns (uint metalCost, uint crystalCost, uint timeUnlocked) {}
 
-    function quarzMineUpgrade(address caller) external returns (uint metalCost, uint crystalCost, uint timeUnlocked) {
+    function tritiumMineUpgrade(
+        address caller
+    ) external returns (uint metalCost, uint crystalCost, uint timeUnlocked) {}
 
-    }
-
-    function tritiumMineUpgrade(address caller) external returns (uint metalCost, uint crystalCost, uint timeUnlocked) {
-
-    }
-
-    function solarPlantUpgrade(address caller) external returns (uint metalCost, uint crystalCost, uint timeUnlocked) {
-
-    }
+    function solarPlantUpgrade(
+        address caller
+    ) external returns (uint metalCost, uint crystalCost, uint timeUnlocked) {}
 
     //#############################################################################################
     //                                   PRIVATE FUNCTIONS                                        #
     // ############################################################################################
 
-    function _metalBuildingCost(uint mineLevel) private returns(uint metalCost, uint crystalCost) {
+    function _metalBuildingCost(
+        uint mineLevel
+    ) private returns (uint metalCost, uint crystalCost) {}
 
-    }
+    function _crystalBuildingCost(
+        uint mineLevel
+    ) private returns (uint metalCost, uint crystalCost) {}
 
-    function _crystalBuildingCost(uint mineLevel) private returns(uint metalCost, uint crystalCost) {
-        
-    }
+    function _deuteriumBuildingCost(
+        uint256 mineLevel
+    ) private returns (uint256 metalCost, uint256 crystalCost) {}
 
-    function _deuteriumBuildingCost(uint256 mineLevel) private returns(uint256 metalCost, uint256 crystalCost) {
-        
-    }
-
-    function _solarBuildingCost(uint256 mineLevel) private returns(uint256 metalCost, uint256 crystalCost) {
-        
-    }
+    function _solarBuildingCost(
+        uint256 mineLevel
+    ) private returns (uint256 metalCost, uint256 crystalCost) {}
 }
