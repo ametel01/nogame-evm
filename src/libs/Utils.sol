6f986a7ea61fbbd61c41441b21e5ff33d0409ff1
Alex Metelli <alex-metelli@gmx.com>
2023-03-29 03:03:37 +0100

    created Utils library

---
+++ src/libs/Utils.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+library Utils {
+    function getAvailableResources(address caller) public returns(uint metal, uint crystal, uint deuterium) {
+
+    }
+
+    function checkEnoughResources(address caller, uint metalRequired, uint crystalRequired, uint deuteriumRequired) public returns(bool) {
+
+    }
+}
