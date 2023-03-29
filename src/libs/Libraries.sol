0bfac40956f52d6c25eeb4f904035a04cec53b4f
Alex Metelli <alex-metelli@gmx.com>
2023-03-29 09:10:07 +0100

    added structs

--- src/libs/Libraries.sol
+++ src/libs/Libraries.sol
@@ -9,6 +9,10 @@ library Utils {
     function checkEnoughResources(address caller, uint metalRequired, uint crystalRequired, uint deuteriumRequired) public returns(bool) {
 
     }
+
+    function getTechLevels(address caller) public returns (Structs.TechLevels memory levels) {
+        
+    }
 }
 
 library Structs {
@@ -17,4 +21,62 @@ library Structs {
         uint quarz;
         uint tritium;
     }
+
+    struct ShipsCosts {
+        Cost ligthTrasport;
+        Cost heavyTransport;
+        Cost debrisRemover;
+        Cost sniffer;
+        Cost satellite;
+        Cost swiftFighter;
+        Cost frigate;
+        Cost warship;
+    }
+
+    struct DefenceCosts {
+        Cost missileLauncher;
+        Cost lightPhotonGun;
+        Cost heavyPhotonGun;
+        Cost electronCannon;
+        Cost magneticCannon;
+        Cost plasmaProjector;
+        Cost smallEnergyField;
+        Cost largeEnergyField;
+    }
+
+    struct TechCosts {
+        Cost materialsTech;
+        Cost stellarPhysics;
+        Cost heatEngine;
+        Cost informationTech;
+        Cost powerTech;
+        Cost surveillanceTech;
+        Cost quantumEngine;
+        Cost quantumTech;
+        Cost fusionEngine;
+        Cost ionizationTech;
+        Cost photonicTech;
+        Cost plasmaTech;
+        Cost defenceTech;
+        Cost armamentsTech;
+    }
+
+    struct TechLevels {
+        uint materialsTech;
+        uint stellarPhysics;
+        uint heatEngine;
+        uint informationTech;
+        uint powerTech;
+        uint surveillanceTech;
+        uint quantumEngine;
+        uint quantumTech;
+        uint fusionEngine;
+        uint ionizationTech;
+        uint photonicTech;
+        uint plasmaTech;
+        uint defenceTech;
+        uint armamentsTech;
+    }
+
+
 }
