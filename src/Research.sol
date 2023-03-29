7aa2687643d9bf956ddca8c2e346a66129c8c619
Alex Metelli <alex-metelli@gmx.com>
2023-03-29 09:10:20 +0100

    added functions headers

---
+++ src/Research.sol
@@ -0,0 +1,172 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Utils, Structs} from "./libs/Libraries.sol";
+
+contract Research {
+
+
+    address private noGameAddr;
+
+    constructor(address _noGameAddr) {
+        noGameAddr = _noGameAddr;
+    }
+
+    function techUpgradeCosts(address caller) public view returns(Structs.TechCosts memory costs) {
+        
+    }
+
+    //#############################################################################################
+    //                                   EXTERNAL FUNCTIONS                                       #
+    // ############################################################################################
+
+    function materialsTechUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function stellarPhysicsUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function heatEngineUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function informationTechUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function quantumEngineUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function quantumTechUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function fusionEngineUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function ionizationTechUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function photonicTechUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function plasmaTechUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function defenceTechUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    function armamentsTechUpgrade(address caller, uint currentLevel) external returns(Structs.Cost memory cost) {
+
+    }
+
+    //#############################################################################################
+    //                                   PRIVATE FUNCTIONS                                        #
+    // ############################################################################################
+
+    function _materialsTechUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+
+    }
+
+    function _stellarPhysicsUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _heatEngineUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _informationTechUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _quantumEngineUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _quantumTechUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _fusionEngineUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _ionizationTechUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _photonicTechUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _plasmaTechUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _defenceTechUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+    
+    function _armamentsTechUpgradeCost(uint currentLevel) private returns(Structs.Cost memory cost) {
+        
+    }
+
+    function _materialsTechRequirementsCheck(address caller) private {
+
+    }
+
+    function _stellarPhysicsRequirementsCheck(address caller) private {
+        
+    }
+
+    function _heatEngineRequirementsCheck(address caller) private {
+        
+    }
+
+    function _informationTechRequirementsCheck(address caller) private {
+        
+    }
+
+    function _quantumEngineRequirementsCheck(address caller) private {
+        
+    }
+
+    function _quantumTechRequirementsCheck(address caller) private {
+        
+    }
+
+    function _fusionEngineRequirementsCheck(address caller) private {
+        
+    }
+
+    function _ionizationTechRequirementsCheck(address caller) private {
+        
+    }
+
+    function _photonicTechRequirementsCheck(address caller) private {
+        
+    }
+
+    function _plasmaTechRequirementsCheck(address caller) private {
+        
+    }
+
+    function _defenceTechRequirementsCheck(address caller) private {
+        
+    }
+
+    function _armamentsTechRequirementsCheck(address caller) private {
+        
+    }
+}
+
+
