601bb3d0902a0bcbf67cba001d3d163737837b47
Alex Metelli <alex-metelli@gmx.com>
2023-03-29 12:45:35 +0100

    chore run formatter

--- src/Facilities.sol
+++ src/Facilities.sol
@@ -4,62 +4,62 @@ pragma solidity ^0.8.13;
 import {Utils} from "./libs/Libraries.sol";
 
 contract Facilities {
-
     address private noGameAddr;
 
     constructor(address _noGameAddr) {
         noGameAddr = _noGameAddr;
     }
 
-    function upgradesCost(address caller) public view returns(uint robotFactory, uint shipyard, uint reasearchLab, uint naniteFactory) {
-        
-    }
+    function upgradesCost(
+        address caller
+    )
+        public
+        view
+        returns (
+            uint robotFactory,
+            uint shipyard,
+            uint reasearchLab,
+            uint naniteFactory
+        )
+    {}
 
     //#############################################################################################
     //                                   EXTERNAL FUNCTIONS                                       #
     // ############################################################################################
 
-    function automationPlantUpgrade(address caller) external {
+    function automationPlantUpgrade(address caller) external {}
 
-    }
+    function dockyardUpgrade(address caller) external {}
 
-    function dockyardUpgrade(address caller) external {
+    function scienceLabUpgrade(address caller) external {}
 
-    }
-
-    function scienceLabUpgrade(address caller) external {
-
-    }
-
-    function microtechFactoryUpgrade(address caller) external {
-
-    }
+    function microtechFactoryUpgrade(address caller) external {}
 
     //#############################################################################################
     //                                   PRIVATE FUNCTIONS                                        #
     // ############################################################################################
 
-    function _dockyardRequirementsCheck(address caller) private returns(bool) {
+    function _dockyardRequirementsCheck(
+        address caller
+    ) private returns (bool) {}
 
-    }
+    function _microtechRequirementsCheck(
+        address caller
+    ) private returns (bool) {}
 
-    function _microtechRequirementsCheck(address caller) private returns(bool) {
+    function _automationUpgradeCost(
+        uint currentLevel
+    ) private returns (uint metal, uint crystal, uint deuterium) {}
 
-    }
+    function _dockyardUpgradeCost(
+        uint currentLevel
+    ) private returns (uint metal, uint crystal, uint deuterium) {}
 
-    function _automationUpgradeCost(uint currentLevel) private returns(uint metal, uint crystal, uint deuterium) {
+    function _scienceLabUpgradeCost(
+        uint currentLevel
+    ) private returns (uint metal, uint crystal, uint deuterium) {}
 
-    }
-
-    function _dockyardUpgradeCost(uint currentLevel) private returns(uint metal, uint crystal, uint deuterium) {
-
-    }
-
-    function _scienceLabUpgradeCost(uint currentLevel) private returns(uint metal, uint crystal, uint deuterium) {
-
-    }
-
-    function _microtechUpgradeCost(uint currentLevel) private returns(uint metal, uint crystal, uint deuterium) {
-
-    }
+    function _microtechUpgradeCost(
+        uint currentLevel
+    ) private returns (uint metal, uint crystal, uint deuterium) {}
 }
